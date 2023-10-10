3fea6af83a136353fc6bec68c13fbcfccb1b964a
Andrew <andrew.andrew@traveloka.com>
2023-10-10 09:01:29 +0700

    feat: update rpc client comments

--- infrastructure/rpcclient/rpcclient.go
+++ infrastructure/rpcclient/rpcclient.go
@@ -15,31 +15,9 @@ import (
 
 //go:generate mockgen -source=rpcclient.go -destination=rpcclient_mock.go -package=rpcclient
 
+// jsonRpcClient is abstraction of json rpc client
 type jsonRpcClient interface {
 	// Call is used to send a JSON-RPC request to the server endpoint.
-	//
-	// The spec states, that params can only be an array or an object, no primitive values.
-	// So there are a few simple rules to notice:
-	//
-	// 1. no params: params field is omitted. e.g. Call(ctx, "getinfo")
-	//
-	// 2. single params primitive value: value is wrapped in array. e.g. Call(ctx, "getByID", 1423)
-	//
-	// 3. single params value array or object: value is unchanged. e.g. Call(ctx, "storePerson", &Person{Name: "Alex"})
-	//
-	// 4. multiple params values: always wrapped in array. e.g. Call(ctx, "setDetails", "Alex, 35, "Germany", true)
-	//
-	// Examples:
-	//   Call(ctx, "getinfo") -> {"method": "getinfo"}
-	//   Call(ctx, "getPersonId", 123) -> {"method": "getPersonId", "params": [123]}
-	//   Call(ctx, "setName", "Alex") -> {"method": "setName", "params": ["Alex"]}
-	//   Call(ctx, "setMale", true) -> {"method": "setMale", "params": [true]}
-	//   Call(ctx, "setNumbers", []int{1, 2, 3}) -> {"method": "setNumbers", "params": [1, 2, 3]}
-	//   Call(ctx, "setNumbers", 1, 2, 3) -> {"method": "setNumbers", "params": [1, 2, 3]}
-	//   Call(ctx, "savePerson", &Person{Name: "Alex", Age: 35}) -> {"method": "savePerson", "params": {"name": "Alex", "age": 35}}
-	//   Call(ctx, "setPersonDetails", "Alex", 35, "Germany") -> {"method": "setPersonDetails", "params": ["Alex", 35, "Germany"}}
-	//
-	// for more information, see the examples or the unit tests
 	Call(ctx context.Context, method string, params ...interface{}) (*jsonrpc.RPCResponse, error)
 }
 
@@ -61,10 +39,29 @@ func NewClient(rpcClientConfig config.RpcClientConfig) *RpcClient {
 	return client
 }
 
-// Call will call rpc method with given method name and params
+// Call is used to send a JSON-RPC request to the server endpoint.
 //
-// returns rpc response and nil error if success
-// otherwise return empty rpc response and non nil error
+// The spec states, that params can only be an array or an object, no primitive values.
+// So there are a few simple rules to notice:
+//
+// 1. no params: params field is omitted. e.g. Call(ctx, "getinfo")
+//
+// 2. single params primitive value: value is wrapped in array. e.g. Call(ctx, "getByID", 1423)
+//
+// 3. single params value array or object: value is unchanged. e.g. Call(ctx, "storePerson", &Person{Name: "Alex"})
+//
+// 4. multiple params values: always wrapped in array. e.g. Call(ctx, "setDetails", "Alex, 35, "Germany", true)
+//
+// Examples:
+//
+//	Call(ctx, "getinfo") -> {"method": "getinfo"}
+//	Call(ctx, "getPersonId", 123) -> {"method": "getPersonId", "params": [123]}
+//	Call(ctx, "setName", "Alex") -> {"method": "setName", "params": ["Alex"]}
+//	Call(ctx, "setMale", true) -> {"method": "setMale", "params": [true]}
+//	Call(ctx, "setNumbers", []int{1, 2, 3}) -> {"method": "setNumbers", "params": [1, 2, 3]}
+//	Call(ctx, "setNumbers", 1, 2, 3) -> {"method": "setNumbers", "params": [1, 2, 3]}
+//	Call(ctx, "savePerson", &Person{Name: "Alex", Age: 35}) -> {"method": "savePerson", "params": {"name": "Alex", "age": 35}}
+//	Call(ctx, "setPersonDetails", "Alex", 35, "Germany") -> {"method": "setPersonDetails", "params": ["Alex", 35, "Germany"}}
 func (rpcClient *RpcClient) Call(ctx context.Context, method string, params ...interface{}) (*jsonrpc.RPCResponse, error) {
 	cbKey := fmt.Sprintf("%s_%s", rpcClient.config.ServiceName, method)
 	if _, exists := rpcClient.cbConfig[method]; !exists {
